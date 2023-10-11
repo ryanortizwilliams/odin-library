5a76c6906f178b995953b15a0287695d3d437c2f
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-10-11 16:06:26 +0900

    Change print to return in info function.

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ function book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    console.log(
-      `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`
-    );
+    return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`;
   };
 }
 
 const theHobbit = new book("The Hobbit", "J.R.R Tolkien", 295, false);
 
-theHobbit.info();
+console.log(theHobbit.info());
